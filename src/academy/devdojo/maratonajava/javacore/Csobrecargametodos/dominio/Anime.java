da937d836a0736cdbae5a9f128674af44a4ed679
Danielbsilva <danielbsilva_@outlook.com>
2025-04-23 14:44:48 -0300

    estudo: assunto novo: sobrecarga de métodos. Seu uso parece ser importante para ambientes em produção. Foram criadas 2 novas classes (Anime e AnimeTest01) dentro do novo pacote Csobrecargametodos

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,59 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+
+    //sobrecarga de métodos é quando temos 2 funções com o mesmo nome
+    //mas a quantidade ou tipos de parametros diferentes
+    public void init(String nome, String tipo, int episodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return this.tipo = tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return this.episodios = episodios;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
